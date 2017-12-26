722ec1a4908abd63118a7926597c0b175f0fadaf
Lena <chydilove@gmail.com>
2017-12-26 20:52:34 +0300

    html

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express'),
     path= require('path'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    serveStatic = require('serve-static');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var url =require('url');
 // view engine setup
-
+app.get("/", function(req, res) {
+    res.sendfile('index.html')
+});
 app.use('/', express.static('/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use('/send', index);
 app.use('/users', users);
 
@@ -18,7 +22,6 @@ app.get(/^(.+)$/, function(req, res){
     console.log('static file request : ' + req.params);
     res.sendfile( __dirname + req.params[0]);
 });
-
 app.listen(8081,'localhost');
 console.log('server start localhost:8081');
 module.exports = app;
