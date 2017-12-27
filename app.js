8f75636367e433f80957d656c46c804ed6294381
Lena <chydilove@gmail.com>
2017-12-27 16:48:20 +0300

    1

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var express = require('express'),
     serveStatic = require('serve-static');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var url =require('url');
 // view engine setup
 app.get("/", function(req, res) {
@@ -16,12 +15,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/send', index);
-app.use('/users', users);
 
 app.get(/^(.+)$/, function(req, res){
     console.log('static file request : ' + req.params);
     res.sendfile( __dirname + req.params[0]);
 });
-app.listen(8081,'localhost');
-console.log('server start localhost:8081');
+app.listen(8080,'localhost');
+console.log('server start localhost:8080');
 module.exports = app;
